a8a70c055e626204483f3f3f03b7e47980426ee0
Wennielynlorenzana <163943854+Wennielynlorenzana@users.noreply.github.com>
2024-09-30 15:36:26 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
