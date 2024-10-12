ff3398e87985c295861f6137ca2417f4914c77bd
Wennielynlorenzana <163943854+Wennielynlorenzana@users.noreply.github.com>
2024-10-12 13:57:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './Home';
+import Header from './Header';
+import Hero from './Hero';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
+import Footer from './Footer';
+import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
   );
 };
 
